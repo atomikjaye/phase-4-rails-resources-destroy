7ebb0a28e0b1c05c1579ea6999e1426641bf8aa1
Jocelyn Naarden <tzionone@gmail.com>
2022-08-19 17:04:24 -0400

    Finished Assignment ✔

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -32,6 +32,18 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      # render json: bird
+      # 204, sending no content
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+  end
+end
 
   # PATCH /birds/:id/like
   def increment_likes
